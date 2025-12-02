25eef85bf986d5fe2423002fbeef4be9d79b2410
matt <m2618264@gmail.com>
2025-12-02 20:47:44 +0330

    Move the home page into react

--- ecommerce-project/eslint.config.js
+++ ecommerce-project/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react';
 
 export default [
   { ignores: ['dist'] },
@@ -17,17 +18,20 @@ export default [
       },
     },
     plugins: {
+      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ]
