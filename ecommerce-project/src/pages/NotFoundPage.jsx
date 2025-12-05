f7a471168b7bc21910e0e2d4a1441d54dafaadd8
matt <m2618264@gmail.com>
2025-12-05 21:50:50 +0330

    Created 404 not found page

---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header';
+import './NotFoundPage.css';
+
+
+export function NotFoundPage() {
+  return (
+    <>
+      <Header />
+      
+      <div className="not-found-message"></div>
+    </>
+  )
+}
