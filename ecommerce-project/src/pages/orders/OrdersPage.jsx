5e50f262fb10133592fa850d1cbd919dc12e17aa
matt <m2618264@gmail.com>
2025-12-07 16:06:32 +0330

    Seperate into smaller components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router';
-import { Header } from '../components/Header';
-import { formatMoney } from '../utils/money';
+import { Header } from '../../components/Header';
+import { formatMoney } from '../../utils/money';
 import './OrdersPage.css';
-import BuyAgain from '../assets/images/icons/buy-again.png';
+import BuyAgain from '../../assets/images/icons/buy-again.png';
 
 
 
